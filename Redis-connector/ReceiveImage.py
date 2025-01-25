8887481e61dbca6f08bbaf553353ab043d4a585c
George.Daoud@OntarioTechu.net <GodIsLove@3>
2025-01-24 21:09:30 -0500

    fix a typo

--- Redis-connector/ReceiveImage.py
+++ Redis-connector/ReceiveImage.py
@@ -8,7 +8,7 @@ r = redis.Redis(host=ip, port=6379, db=0,password='sofe4630u')
 value=r.get('image');
 decoded_value=base64.b64decode(value);
 
-with open("./recieved.jpg", "wb") as f:
+with open("./received.jpg", "wb") as f:
     f.write(decoded_value);
     
-print('Image recieved, check ./recieved.jpg')
+print('Image received, check ./received.jpg')
